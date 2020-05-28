b35d0d19090850598e6f4bd99c675a7f595f7705
Dmitrii Iarosh <dimajarosh@gmail.com>
2020-05-28 20:27:19 +0300

    fixed GForm entities

--- fs.js
+++ fs.js
@@ -5,151 +5,151 @@ let fs_JSON = [
         files:['1.png', '2.png', '3.png']
     },
 	{
-        entry:'462049852',
+        entry:'1407111109',
         dir:'Data/2',
         files:['1.png', '2.png', '3.png']
     },
 	{
-        entry:'213286826',
+        entry:'1469685396',
         dir:'Data/3',
         files:['1.png', '2.png', '3.png']
     },
     {
-        entry:'1600685037',
+        entry:'711786394',
         dir:'Data/4',
         files:['1.png', '2.png', '3.png']
     },
     {
-        entry:'658915053',
+        entry:'2082578813',
         dir:'Data/5',
         files:['1.png', '2.png', '3.png']
     },
     {
-        entry:'458671234',
+        entry:'2102392066',
         dir:'Data/6',
         files:['1.png', '2.png', '3.png']
     },
     {
-        entry:'2026190367',
+        entry:'855008580',
         dir:'Data/7',
         files:['1.png', '2.png', '3.png']
     },
     {
-        entry:'141722385',
+        entry:'1615996774',
         dir:'Data/8',
         files:['1.png', '2.png', '3.png']
     },
     {
-        entry:'1316133051',
+        entry:'1613486169',
         dir:'Data/9',
         files:['1.png', '2.png', '3.png']
     },
     {
-        entry:'667588053',
+        entry:'829838881',
         dir:'Data/10',
         files:['1.png', '2.png', '3.png']
     },
     {
-        entry:'1730484008',
+        entry:'956549456',
         dir:'Data/11',
         files:['1.png', '2.png', '3.png']
     },
     {
-        entry:'829430896',
+        entry:'1818118270',
         dir:'Data/12',
         files:['1.png', '2.png', '3.png']
     },
     {
-        entry:'1032493526',
+        entry:'1292571273',
         dir:'Data/13',
         files:['1.png', '2.png', '3.png']
     },
     {
-        entry:'1597397185',
+        entry:'1883182950',
         dir:'Data/14',
         files:['1.png', '2.png', '3.png']
     },
     {
-        entry:'1004969227',
+        entry:'1746062661',
         dir:'Data/15',
         files:['1.png', '2.png', '3.png']
     },
     {
-        entry:'1702819562',
+        entry:'31525506',
         dir:'Data/16',
         files:['1.png', '2.png', '3.png']
     },
     {
-        entry:'757159913',
+        entry:'756310136',
         dir:'Data/17',
         files:['1.png', '2.png', '3.png']
     },
     {
-        entry:'1188014235',
+        entry:'1516935354',
         dir:'Data/18',
         files:['1.png', '2.png', '3.png']
     },
     {
-        entry:'1787990840',
+        entry:'1583608001',
         dir:'Data/19',
         files:['1.png', '2.png', '3.png']
     },
     {
-        entry:'1127937521',
+        entry:'704212951',
         dir:'Data/20',
         files:['1.png', '2.png', '3.png']
     },
     {
-        entry:'47714939',
+        entry:'1093535961',
         dir:'Data/21',
         files:['1.png', '2.png', '3.png']
     },
     {
-        entry:'662256874',
+        entry:'997989049',
         dir:'Data/22',
         files:['1.png', '2.png', '3.png']
     },
     {
-        entry:'1760000826',
+        entry:'668199272',
         dir:'Data/23',
         files:['1.png', '2.png', '3.png']
     },
     {
-        entry:'2106361910',
+        entry:'981939758',
         dir:'Data/24',
         files:['1.png', '2.png', '3.png']
     },
     {
-        entry:'1318164553',
+        entry:'459894570',
         dir:'Data/25',
         files:['1.png', '2.png', '3.png']
     },
     {
-        entry:'1815015169',
+        entry:'1534298014',
         dir:'Data/26',
         files:['1.png', '2.png', '3.png']
     },
     {
-        entry:'53333538',
+        entry:'221658296',
         dir:'Data/27',
         files:['1.png', '2.png', '3.png']
     },
     {
-        entry:'1210841745',
+        entry:'1008368269',
         dir:'Data/28',
         files:['1.png', '2.png', '3.png']
     },
     {
-        entry:'836180393',
+        entry:'1277191581',
         dir:'Data/29',
         files:['1.png', '2.png', '3.png']
     },
 	{
-        entry:'1878597372',
+        entry:'1149067583',
         dir:'Data/30',
         files:['1.png', '2.png', '3.png']
     },
 ]
-let platformEntry ='274139876'
-let versionEntry ='454061805'
+let platformEntry ='749742724'
+let versionEntry ='459513751'
 let formId ='1FAIpQLSeRlDcLZr77TAkQp-t50JND2xRCBQPIypr37UJK3Woqlv3sXw'
