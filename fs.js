0963f52c6c5b5374a541a03bb7edd1652ee2f0f9
Dmitrii Iarosh <dimajarosh@gmail.com>
2020-05-28 21:12:44 +0300

    renamed samples

--- fs.js
+++ fs.js
@@ -2,152 +2,152 @@ let fs_JSON = [
     {
         entry:'2087134877',
         dir:'Data/1',
-        files:['1.png', '2.png', '3.png']
+        files:['cropped_gt.png', 'results_max.png', 'results_mean.png']
     },
 	{
-        entry:'1407111109',
+        entry:'1568110855',
         dir:'Data/2',
-        files:['1.png', '2.png', '3.png']
+        files:['results_max.png', 'cropped_gt.png', 'results_mean.png']
     },
 	{
-        entry:'1469685396',
+        entry:'92932988',
         dir:'Data/3',
-        files:['1.png', '2.png', '3.png']
+        files:['cropped_gt.png', 'results_mean.png', 'results_max.png']
     },
-    {
-        entry:'711786394',
+	    {
+        entry:'1201273054',
         dir:'Data/4',
-        files:['1.png', '2.png', '3.png']
+        files:['results_mean.png', 'cropped_gt.png', 'results_max.png']
     },
     {
-        entry:'2082578813',
+        entry:'1145533237',
         dir:'Data/5',
-        files:['1.png', '2.png', '3.png']
+        files:['cropped_gt.png', 'results_max.png', 'results_mean.png']
     },
     {
-        entry:'2102392066',
+        entry:'1257630578',
         dir:'Data/6',
-        files:['1.png', '2.png', '3.png']
+        files:['results_max.png', 'cropped_gt.png', 'results_mean.png']
     },
     {
-        entry:'855008580',
+        entry:'653812323',
         dir:'Data/7',
-        files:['1.png', '2.png', '3.png']
+        files:['results_mean.png', 'cropped_gt.png', 'results_max.png']
     },
     {
-        entry:'1615996774',
+        entry:'1408294189',
         dir:'Data/8',
-        files:['1.png', '2.png', '3.png']
+        files:['cropped_gt.png', 'results_mean.png', 'results_max.png']
     },
     {
-        entry:'1613486169',
+        entry:'33452074',
         dir:'Data/9',
-        files:['1.png', '2.png', '3.png']
+        files:['results_max.png', 'cropped_gt.png', 'results_mean.png']
     },
     {
-        entry:'829838881',
+        entry:'23125357',
         dir:'Data/10',
-        files:['1.png', '2.png', '3.png']
+        files:['results_mean.png', 'cropped_gt.png', 'results_max.png']
     },
     {
-        entry:'956549456',
+        entry:'1971163683',
         dir:'Data/11',
-        files:['1.png', '2.png', '3.png']
+        files:['cropped_gt.png', 'results_max.png', 'results_mean.png']
     },
     {
-        entry:'1818118270',
+        entry:'1088969994',
         dir:'Data/12',
-        files:['1.png', '2.png', '3.png']
+        files:['results_max.png', 'cropped_gt.png', 'results_mean.png']
     },
     {
-        entry:'1292571273',
+        entry:'1086992054',
         dir:'Data/13',
-        files:['1.png', '2.png', '3.png']
+        files:['results_mean.png', 'cropped_gt.png', 'results_max.png']
     },
     {
-        entry:'1883182950',
+        entry:'339475491',
         dir:'Data/14',
-        files:['1.png', '2.png', '3.png']
+        files:['cropped_gt.png', 'results_mean.png', 'results_max.png']
     },
     {
-        entry:'1746062661',
+        entry:'1365733308',
         dir:'Data/15',
-        files:['1.png', '2.png', '3.png']
+        files:['cropped_gt.png', 'results_max.png', 'results_mean.png']
     },
     {
-        entry:'31525506',
+        entry:'495402268',
         dir:'Data/16',
-        files:['1.png', '2.png', '3.png']
+        files:['results_max.png', 'cropped_gt.png', 'results_mean.png']
     },
     {
-        entry:'756310136',
+        entry:'1095873406',
         dir:'Data/17',
-        files:['1.png', '2.png', '3.png']
+        files:['results_mean.png', 'cropped_gt.png', 'results_max.png']
     },
     {
-        entry:'1516935354',
+        entry:'773703489',
         dir:'Data/18',
-        files:['1.png', '2.png', '3.png']
+        files:['cropped_gt.png', 'results_max.png', 'results_mean.png']
     },
     {
-        entry:'1583608001',
+        entry:'1210478793',
         dir:'Data/19',
-        files:['1.png', '2.png', '3.png']
+        files:['cropped_gt.png', 'results_mean.png', 'results_max.png']
     },
     {
-        entry:'704212951',
+        entry:'10079621',
         dir:'Data/20',
-        files:['1.png', '2.png', '3.png']
+        files:['results_mean.png', 'cropped_gt.png', 'results_max.png']
     },
     {
-        entry:'1093535961',
+        entry:'1357058540',
         dir:'Data/21',
-        files:['1.png', '2.png', '3.png']
+        files:['cropped_gt.png', 'results_max.png', 'results_mean.png']
     },
     {
-        entry:'997989049',
+        entry:'1870684666',
         dir:'Data/22',
-        files:['1.png', '2.png', '3.png']
+        files:['results_mean.png', 'cropped_gt.png', 'results_max.png']
     },
     {
-        entry:'668199272',
+        entry:'2051100537',
         dir:'Data/23',
-        files:['1.png', '2.png', '3.png']
+        files:['cropped_gt.png', 'results_mean.png', 'results_max.png']
     },
     {
-        entry:'981939758',
+        entry:'1809959481',
         dir:'Data/24',
-        files:['1.png', '2.png', '3.png']
+        files:['results_max.png', 'cropped_gt.png', 'results_mean.png']
     },
     {
-        entry:'459894570',
+        entry:'487998332',
         dir:'Data/25',
-        files:['1.png', '2.png', '3.png']
+        files:['cropped_gt.png', 'results_max.png', 'results_mean.png']
     },
     {
-        entry:'1534298014',
+        entry:'35577729',
         dir:'Data/26',
-        files:['1.png', '2.png', '3.png']
+        files:['results_mean.png', 'cropped_gt.png', 'results_max.png']
     },
     {
-        entry:'221658296',
+        entry:'946409443',
         dir:'Data/27',
-        files:['1.png', '2.png', '3.png']
+        files:['cropped_gt.png', 'results_mean.png', 'results_max.png']
     },
     {
-        entry:'1008368269',
+        entry:'667175420',
         dir:'Data/28',
-        files:['1.png', '2.png', '3.png']
+        files:['results_max.png', 'cropped_gt.png', 'results_mean.png']
     },
     {
-        entry:'1277191581',
+        entry:'1134849637',
         dir:'Data/29',
-        files:['1.png', '2.png', '3.png']
+        files:['results_mean.png', 'cropped_gt.png', 'results_max.png']
     },
-	{
-        entry:'1149067583',
+    {
+        entry:'201112149',
         dir:'Data/30',
-        files:['1.png', '2.png', '3.png']
+        files:['cropped_gt.png', 'results_mean.png', 'results_max.png']
     },
 ]
 let platformEntry ='749742724'
