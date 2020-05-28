3bd085be43d58e35c5042232f7a70576591554d9
Dmitrii Iarosh <dimajarosh@gmail.com>
2020-05-28 20:00:41 +0300

    Webpage added

---
+++ fs.js
@@ -0,0 +1,155 @@
+let fs_JSON = [
+    {
+        entry:'2087134877',
+        dir:'Data\1',
+        files:['1.png', '2.png', '3.png']
+    },
+	{
+        entry:'462049852',
+        dir:'Data\2',
+        files:['1.png', '2.png', '3.png']
+    },
+	{
+        entry:'213286826',
+        dir:'Data\3',
+        files:['1.png', '2.png', '3.png']
+    },
+    {
+        entry:'1600685037',
+        dir:'Data\4',
+        files:['1.png', '2.png', '3.png']
+    },
+    {
+        entry:'658915053',
+        dir:'Data\5',
+        files:['1.png', '2.png', '3.png']
+    },
+    {
+        entry:'458671234',
+        dir:'Data\6',
+        files:['1.png', '2.png', '3.png']
+    },
+    {
+        entry:'2026190367',
+        dir:'Data\7',
+        files:['1.png', '2.png', '3.png']
+    },
+    {
+        entry:'141722385',
+        dir:'Data\8',
+        files:['1.png', '2.png', '3.png']
+    },
+    {
+        entry:'1316133051',
+        dir:'Data\9',
+        files:['1.png', '2.png', '3.png']
+    },
+    {
+        entry:'667588053',
+        dir:'Data\10',
+        files:['1.png', '2.png', '3.png']
+    },
+    {
+        entry:'1730484008',
+        dir:'Data\11',
+        files:['1.png', '2.png', '3.png']
+    },
+    {
+        entry:'829430896',
+        dir:'Data\12',
+        files:['1.png', '2.png', '3.png']
+    },
+    {
+        entry:'1032493526',
+        dir:'Data\13',
+        files:['1.png', '2.png', '3.png']
+    },
+    {
+        entry:'1597397185',
+        dir:'Data\14',
+        files:['1.png', '2.png', '3.png']
+    },
+    {
+        entry:'1004969227',
+        dir:'Data\15',
+        files:['1.png', '2.png', '3.png']
+    },
+    {
+        entry:'1702819562',
+        dir:'Data\16',
+        files:['1.png', '2.png', '3.png']
+    },
+    {
+        entry:'757159913',
+        dir:'Data\17',
+        files:['1.png', '2.png', '3.png']
+    },
+    {
+        entry:'1188014235',
+        dir:'Data\18',
+        files:['1.png', '2.png', '3.png']
+    },
+    {
+        entry:'1787990840',
+        dir:'Data\19',
+        files:['1.png', '2.png', '3.png']
+    },
+    {
+        entry:'1127937521',
+        dir:'Data\20',
+        files:['1.png', '2.png', '3.png']
+    },
+    {
+        entry:'47714939',
+        dir:'Data\21',
+        files:['1.png', '2.png', '3.png']
+    },
+    {
+        entry:'662256874',
+        dir:'Data\22',
+        files:['1.png', '2.png', '3.png']
+    },
+    {
+        entry:'1760000826',
+        dir:'Data\23',
+        files:['1.png', '2.png', '3.png']
+    },
+    {
+        entry:'2106361910',
+        dir:'Data\24',
+        files:['1.png', '2.png', '3.png']
+    },
+    {
+        entry:'1318164553',
+        dir:'Data\25',
+        files:['1.png', '2.png', '3.png']
+    },
+    {
+        entry:'1815015169',
+        dir:'Data\26',
+        files:['1.png', '2.png', '3.png']
+    },
+    {
+        entry:'53333538',
+        dir:'Data\27',
+        files:['1.png', '2.png', '3.png']
+    },
+    {
+        entry:'1210841745',
+        dir:'Data\28',
+        files:['1.png', '2.png', '3.png']
+    },
+    {
+        entry:'836180393',
+        dir:'Data\29',
+        files:['1.png', '2.png', '3.png']
+    },
+	{
+        entry:'1878597372',
+        dir:'Data\30',
+        files:['1.png', '2.png', '3.png']
+    },
+]
+let platformEntry ='274139876'
+let versionEntry ='454061805'
+let formId ='1FAIpQLSeRlDcLZr77TAkQp-t50JND2xRCBQPIypr37UJK3Woqlv3sXw'
